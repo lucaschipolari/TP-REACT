752ea4c89cdbd7ce62c2669ce511cd65cb6b778b
lucas <zlucaschipolariz@gmail.com>
2024-06-11 13:33:40 -0300

    hice el ejercicio 2

--- tp-react-1/src/App.jsx
+++ tp-react-1/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Ejercicio1 } from '../components/ejercicio1'
 import './App.css'
+import { Ejercicio2 } from '../components/ejercicio2'
 
 function App() {
 
   return (
     <>
-    <Ejercicio1></Ejercicio1>
+    <Ejercicio1/>
+    <Ejercicio2 mensaje='My Friend'/>
     </>
   )
 }
