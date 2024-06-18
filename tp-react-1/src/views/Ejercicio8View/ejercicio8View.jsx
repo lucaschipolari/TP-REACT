1b1bbe4121eb248e872879b7c3d233fea6fd214d
lucas <zlucaschipolariz@gmail.com>
2024-06-18 08:30:15 -0300

    termine el ejercicio 8

---
+++ tp-react-1/src/views/Ejercicio8View/ejercicio8View.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Formulario from '../../exercises/Ejercicio8/formulario'
+
+const Ejercicio8View = () => {
+  return (
+    <div className='container d-flex justify-content-center my-5'>
+    <div className='d-flex flex-column w-50 '>
+    <Formulario/>
+    </div>
+
+    </div>
+  )
+}
+
+export default Ejercicio8View
